07b882186a5f01252ef746ffe60ac3ae68d5065c
Kojanzy <nwmarinoto@gmail.com>
2023-11-04 16:19:55 -0400

    initial config files

--- src/bot-modu.ts
+++ src/bot-modu.ts
@@ -1,5 +1,12 @@
-export class BotModule {
-    inj(): void {
+import { DependencyContainer } from "tsyringe";
 
+export class BotModule
+{
+    inj(container: DependencyContainer, config: any): void
+    {
+        const configServer = container.resolve<any>("ConfigServer");
+        const pmc = configServer.getConfig("aki-pmc")
+
+        pmc.chanceSameSideIsHostilePercent = config["USEC_BEAR_RATIO"]
     }
 }
--- src/mod.ts
+++ src/mod.ts
@@ -1,24 +1,20 @@
 import { DependencyContainer } from "tsyringe";
-import { ConfigServer } from "@spt-aki/servers/ConfigServer";
-import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
-import { IPostAkiLoadMod } from "@spt-aki/models/external/IPostAkiLoadMod";
+//import { ConfigServer } from "@spt-aki/servers/ConfigServer";
+//import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
 import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 
-import * as path from "path";
-import * as fs from "fs";
-import * as config from "./../config.json";
+//import * as path from "path";
+//import * as fs from "fs";
+//import * as config from "./../config/config.json";
+import * as aiConfig from "../config/ai.json";
 
 import { BotModule } from "./bot-modu";
+const botModule = new BotModule();
 
-export class Mod implements IPostAkiLoadMod, IPostDBLoadMod
+export class Mod implements IPostDBLoadMod
 {
     public postDBLoad(container: DependencyContainer): void
     {
-        
-    }
-
-    public postAkiLoad(container: DependencyContainer): void
-    {
-        
+        botModule.inj(container, aiConfig);
     }
 }
