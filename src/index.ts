7662f6093ac99c327ff888e8ad2ddbef3f20916d
Kojanzy <nwmarinoto@gmail.com>
2023-12-13 20:23:08 -0500

    rewrite config conditions for `sBase` overrider module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { DependencyContainer } from "tsyringe";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 
-import * as config from "./config/base.json";
-import * as gameConfig from "./config/game.json";
-import * as miscConfig from "./config/misc.json";
-import * as npcConfig from "./config/npc.json";
+import * as config from "../config/data.json";
 
 import sBase from "./overrides/sBase";
 import sAirdrop from "./overrides/sAirdrop";
@@ -18,20 +15,12 @@ export default class Index
 {
     inj(container: DependencyContainer): void
     {
-        // resolve log
-        const logger = container.resolve<ILogger>("WinstonLogger")
-        if (!config["ENABLED"]) // incase shoal is disabled...
-        {
-            logger.info("shoal was unable to inject.");
-            return;
-        } // run all patches according to corresp. cfg
-        sBase.exec(container, config, gameConfig);
-        sAirdrop.exec(container, miscConfig);
-        sBoss.exec(container, npcConfig);
-        sBot.exec(container, npcConfig);
+        sBase.exec(container, config);
+        sAirdrop.exec(container, config);
+        sBoss.exec(container, config);
+        sBot.exec(container, config);
         sHideout.exec(container, config);
-        sPmc.exec(container, npcConfig);
-        if (!gameConfig["EXTRACT_MASTER"]) return;
-        new ExitHandler(); // handle exits
+        sPmc.exec(container, config);
+        new ExitHandler();
     }
 }
--- src/overrides/sBase.ts
+++ src/overrides/sBase.ts
@@ -5,57 +5,60 @@ import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 // general gameplay changes
 export default class sBase
 {
-    static exec(container: DependencyContainer, baseConfig: any, gameConfig: any): void
+    static exec(container: DependencyContainer, config: any): void
     {
         const tables = container.resolve<DatabaseServer>("DatabaseServer").getTables();
         const globals = tables.globals.config;
         const quests = tables.templates.quests;
         const bodyPartHealth = globals.Health.ProfileHealthSettings.BodyPartsSettings;
 
-        globals.TimeBeforeDeploy = baseConfig["TIME_TO_DEPLOY"];
-        globals.TimeBeforeDeployLocal = baseConfig["TIME_TO_DEPLOY"];
+        globals.TimeBeforeDeploy = config.TimeToDeploy;
+        globals.TimeBeforeDeployLocal = config.TimeToDeploy;
 
-        globals.SavagePlayCooldown = gameConfig["SCAV_COOLDOWN"];
+        globals.SavagePlayCooldown = config.ScavengerModeCooldown;
 
-        globals.exp.match_end.survivedMult = gameConfig["SURVIVED_EXP_MULT"];
-        globals.exp.match_end.killedMult = gameConfig["KILLED_EXP_MULT"];
+        globals.exp.match_end.survivedMult = config.ExtractExperienceMultiplier;
+        globals.exp.match_end.killedMult = config.KilledExperienceMultiplier;
 
-        bodyPartHealth.Head.Maximum = gameConfig["HEAD_HP"];
-        bodyPartHealth.Chest.Maximum = gameConfig["CHEST_HP"];
-        bodyPartHealth.Stomach.Maximum = gameConfig["STOMACH_HP"];
-        bodyPartHealth.LeftArm.Maximum = gameConfig["LARM_HP"];
-        bodyPartHealth.RightArm.Maximum = gameConfig["RARM_HP"];
-        bodyPartHealth.LeftLeg.Maximum = gameConfig["LLEG_HP"];
-        bodyPartHealth.RightLeg.Maximum = gameConfig["RLEG_HP"];
+        bodyPartHealth.Head.Maximum = config.HeadHitpoints;
+        bodyPartHealth.Chest.Maximum = config.ChestHitpoints;
+        bodyPartHealth.Stomach.Maximum = config.StomachHitpoints;
+        bodyPartHealth.LeftArm.Maximum = config.LeftArmHitpoints;
+        bodyPartHealth.RightArm.Maximum = config.RightArmHitpoints;
+        bodyPartHealth.LeftLeg.Maximum = config.LeftLegHitpoints;
+        bodyPartHealth.RightLeg.Maximum = config.RightLegHitpoints;
 
-        if (!gameConfig["SKILL_FATIGUE"])
+        if (config.DisableSkillFatigue)
         {
             globals.SkillFatiguePerPoint = 0;
             globals.SkillMinEffectiveness = 1.0;
             globals.SkillFreshEffectiveness = 1.0;
         }
 
-        if (gameConfig["DISABLE_FALL_DMG"])
+        if (config.DisableFallDamage)
             globals.Health.Falling.DamagePerMeter = 0;
 
-        if (gameConfig["FREE_LAB_ACCESS"])
+        if (config.AccessLabsWithoutCard)
             tables.locations.laboratory.base.AccessKeys = [];
 
-        if (gameConfig["DISABLE_SCAV_MODE"])
+        if (config.DisableScavengerMode)
+        {
             for (const map in tables.locations)
                 tables.locations[map].base.DisabledForScav = true;
+            globals.SavagePlayCooldown = 80000;
+        }
 
-        if (gameConfig["EXTEND_RAID_TIMER"])
+        if (config.ExtendRaidTimerToHour)
             for (const map in tables.locations)
                 tables.locations[map].base.EscapeTimeLimit = 60;
 
-        if (gameConfig["DISABLE_RUNTHRUS"])
+        if (config.DisableRunthroughs)
         {
             globals.exp.match_end.survived_exp_requirement = 0;
             globals.exp.match_end.survived_seconds_requirement = 0;
         }
 
-        if (gameConfig["REMOVE_FIR_CONDITION"])
+        if (config.RemoveFoundInRaidQuestConditions)
             for (const quest in quests)
                 for (const condition in quest["conditions"]["AvailableForFinish"])
                     if (condition["_parent"] == "HandoverItem" && !condition["_props"].onlyFoundInRaid)
