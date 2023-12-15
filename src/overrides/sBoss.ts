899ba0d3b5adc7eae24ecb20a991b47bea902225
Kojanzy <nwmarinoto@gmail.com>
2023-12-14 23:06:37 -0500

    refactor `sBoss` module config usage to be inline with gui output

--- src/overrides/sBoss.ts
+++ src/overrides/sBoss.ts
@@ -2,7 +2,6 @@
 import { DependencyContainer } from "tsyringe";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-// run tweaks on boss chance, location(s), and follower counts
 export default class sBoss
 {
     static exec(container: DependencyContainer, config: any): void
@@ -18,57 +17,31 @@ export default class sBoss
         const lighthouse = tables.locations["lighthouse"].base;
         const streets = tables.locations["tarkovstreets"].base;
 
-        customs.BossLocationSpawn[0].BossChance = config["CUSTOMS_GOONS"][0];
-
-        customs.BossLocationSpawn[1].BossChance = config["RESHALA"][0];
-        customs.BossLocationSpawn[1].BossEscortAmount = config["RESHALA"][1];
-
-        customs.BossLocationSpawn[2].BossChance = config["CUSTOMS_CULTIST"][0];
-        customs.BossLocationSpawn[2].BossEscortAmount = config["CUSTOMS_CULTIST"][1];
-
-        woods.BossLocationSpawn[0].BossChance = config["WOODS_GOONS"][0];
-
-        woods.BossLocationSpawn[1].BossChance = config["SHTURMAN"][0];
-        woods.BossLocationSpawn[1].BossEscortAmount = config["SHTURMAN"][1];
-
-        woods.BossLocationSpawn[2].BossChance = config["WOODS_CULTIST"][0];
-        woods.BossLocationSpawn[2].BossEscortAmount = config["WOODS_CULTIST"][1];
-        woods.BossLocationSpawn[2].BossZone = config["WOODS_CULTIST"][2];
-
-        interchange.BossLocationSpawn[0].BossChance = config["INTERCHANGE_KILLA"][0];
-
-        factoryDay.BossLocationSpawn[0].BossChance = config["FACTORY_DAY_TAGILLA"][0];
-
-        factoryNight.BossLocationSpawn[0].BossChance = config["FACTORY_NIGHT_TAGILLA"][0];
-
-        factoryNight.BossLocationSpawn[1].BossChance = config["FACTORY_NIGHT_CULTIST"][0];
-        factoryNight.BossLocationSpawn[1].BossEscortAmount = config["FACTORY_NIGHT_CULTIST"][1];
-
-        lighthouse.BossLocationSpawn[0].BossChance = config["ZRYACHIY"][0];
-        lighthouse.BossLocationSpawn[0].BossEscortAmount = config["ZRYACHIY"][1];
-
-        lighthouse.BossLocationSpawn[2].BossChance = config["LIGHTHOUSE_GOONS"][0];
-        lighthouse.BossLocationSpawn[2].BossZone = config["LIGHTHOUSE_GOONS"][1];
-
-        reserve.BossLocationSpawn[0].BossChance = config["GLUKHAR"][0];
-        reserve.BossLocationSpawn[0].Supports[0].BossEscortAmount = config["GLUKHAR"][1];
-        reserve.BossLocationSpawn[0].Supports[1].BossEscortAmount = config["GLUKHAR"][2];
-        reserve.BossLocationSpawn[0].Supports[2].BossEscortAmount = config["GLUKHAR"][0];
-
-        shoreline.BossLocationSpawn[2].BossChance = config["SANITAR"][0];
-        shoreline.BossLocationSpawn[2].BossEscortAmount = config["SANITAR"][1];
-        shoreline.BossLocationSpawn[2].BossZone = config["SANITAR"][2];
-
-        shoreline.BossLocationSpawn[1].BossChance = config["SHORELINE_GOONS"][0];
-
-        shoreline.BossLocationSpawn[3].BossChance = config["SHORELINE_CULTIST"][0];
-        shoreline.BossLocationSpawn[4].BossChance = config["SHORELINE_CULTIST"][0];
-        shoreline.BossLocationSpawn[3].BossEscortAmount = config["SHORELINE_CULTIST"][1];
-        shoreline.BossLocationSpawn[4].BossEscortAmount = config["SHORELINE_CULTIST"][1];
-
-        streets.BossLocationSpawn[0].BossChance = config["KABAN"][0];
-        streets.BossLocationSpawn[0].BossEscortAmount = config["KABAN"][1];
-
-        streets.BossLocationSpawn[1].BossChance = config["STREETS_KILLA"][0];
+        customs.BossLocationSpawn[0].BossChance = +config.CustomsGoonsquadBoss[0];
+        customs.BossLocationSpawn[1].BossChance = +config.CustomsReshalaBoss[0];
+        customs.BossLocationSpawn[1].BossEscortAmount = config.CustomsReshalaBoss[1];
+        customs.BossLocationSpawn[2].BossChance = +config.CustomsCultistBoss[0];
+        woods.BossLocationSpawn[0].BossChance = +config.WoodsGoonsquadBoss[0];
+        woods.BossLocationSpawn[1].BossChance = +config.WoodsShturmanBoss[0];
+        woods.BossLocationSpawn[1].BossEscortAmount = config.WoodsShturmanBoss[1];
+        woods.BossLocationSpawn[2].BossChance = +config.WoodsCultistBoss[0];
+        interchange.BossLocationSpawn[0].BossChance = +config.InterchangeKillaBoss[0];
+        factoryDay.BossLocationSpawn[0].BossChance = +config.DayFactoryTagillaBoss[0];
+        factoryNight.BossLocationSpawn[0].BossChance = +config.NightFactoryTagillaBoss[0];
+        factoryNight.BossLocationSpawn[1].BossChance = +config.NightFactoryCultistBoss[0];
+        lighthouse.BossLocationSpawn[2].BossChance = +config.LighthouseGoonsquadBoss[0];
+        reserve.BossLocationSpawn[0].BossChance = +config.ReserveGlukharBoss[0];
+        reserve.BossLocationSpawn[0].Supports[0].BossEscortAmount = config.ReserveGlukharBoss[1];
+        reserve.BossLocationSpawn[0].Supports[1].BossEscortAmount = config.ReserveGlukharBoss[2];
+        reserve.BossLocationSpawn[0].Supports[2].BossEscortAmount = config.ReserveGlukharBoss[0];
+        shoreline.BossLocationSpawn[2].BossChance = +config.ShorelineSanitarBoss[0];
+        shoreline.BossLocationSpawn[2].BossEscortAmount = config.ShorelineSanitarBoss[1];
+        shoreline.BossLocationSpawn[2].BossZone = config.ShorelineSanitarBoss[2];
+        shoreline.BossLocationSpawn[1].BossChance = +config.ShorelineGoonsquadBoss[0];
+        shoreline.BossLocationSpawn[3].BossChance = +config.ShorelineCultistBoss[0];
+        shoreline.BossLocationSpawn[4].BossChance = +config.ShorelineCultistBoss[0];
+        streets.BossLocationSpawn[0].BossChance = +config.StreetsKabanBoss[0];
+        streets.BossLocationSpawn[0].BossEscortAmount = config.StreetsKabanBoss[1];
+        streets.BossLocationSpawn[1].BossChance = +config.StreetsKillaBoss[0];
     }
 }
