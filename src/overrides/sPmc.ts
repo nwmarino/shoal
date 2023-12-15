3d6f389ed2b6cdaa789f81cd1516eab7703d159f
Kojanzy <nwmarinoto@gmail.com>
2023-12-14 23:14:56 -0500

    refactored `sPmc` module to be more inline with gui output

--- src/overrides/sPmc.ts
+++ src/overrides/sPmc.ts
@@ -5,7 +5,6 @@ import { ConfigTypes } from "@spt-aki/models/enums/ConfigTypes";
 import { IPmcConfig } from "@spt-aki/models/spt/config/IPmcConfig";
 import { ILocationConfig } from "@spt-aki/models/spt/config/ILocationConfig";
 
-// pmc bot related tweaks
 export default class sPmc
 {
     static exec(container: DependencyContainer, config: any): void
@@ -13,19 +12,18 @@ export default class sPmc
         const configServer = container.resolve<ConfigServer>("ConfigServer");
         const pmc = configServer.getConfig<IPmcConfig>(ConfigTypes.PMC);
         const location = configServer.getConfig<ILocationConfig>(ConfigTypes.LOCATION);
-        pmc.isUsec = config["USEC_BEAR_RATIO"];
-        pmc.chanceSameSideIsHostilePercent = config["FRIENDLY_PMC_PERC"];
-        pmc.maxBackpackLootTotalRub = config["MAX_BACKPACK_VALUE"];
-        pmc.maxPocketLootTotalRub = config["MAX_POCKET_VALUE"];
-        pmc.maxVestLootTotalRub = config["MAX_VEST_VALUE"];
-        pmc.convertIntoPmcChance.assault.min = config["SCAV_TO_PMC"][0];
-        pmc.convertIntoPmcChance.assault.max = config["SCAV_TO_PMC"][1];
-        pmc.convertIntoPmcChance.cursedassault.min = config["CURSED_TO_PMC"][0];
-        pmc.convertIntoPmcChance.cursedassault.max = config["CURSED_TO_PMC"][1];
-        pmc.convertIntoPmcChance.pmcbot.min = config["RAIDER_TO_PMC"][0];
-        pmc.convertIntoPmcChance.pmcbot.max = config["RAIDER_TO_PMC"][1];
-        pmc.convertIntoPmcChance.exusec.min = config["ROGUE_TO_PMC"][0];
-        pmc.convertIntoPmcChance.exusec.max = config["ROGUE_TO_PMC"][1];
-        location.addCustomBotWavesToMaps = config["BOT_WAVES"];
+        pmc.isUsec = config.UsecToBearRatio;
+        pmc.chanceSameSideIsHostilePercent = config.PmcFriendlyChance;
+        pmc.maxBackpackLootTotalRub = config.PmcBackpackMaxValue;
+        pmc.maxVestLootTotalRub = config.PmcVestMaxValue;
+        pmc.convertIntoPmcChance.assault.min = config.ScavengerToPmc;
+        pmc.convertIntoPmcChance.assault.max = config.ScavengerToPmc;
+        pmc.convertIntoPmcChance.cursedassault.min = config.ScavengerToPmc;
+        pmc.convertIntoPmcChance.cursedassault.max = config.ScavengerToPmc;
+        pmc.convertIntoPmcChance.pmcbot.min = config.RaiderToPmc;
+        pmc.convertIntoPmcChance.pmcbot.max = config.RaiderToPmc;
+        pmc.convertIntoPmcChance.exusec.min = config.RogueToPmc;
+        pmc.convertIntoPmcChance.exusec.max = config.RogueToPmc;
+        location.addCustomBotWavesToMaps = !config.DisableCustomBotWaves;
     }
 }
