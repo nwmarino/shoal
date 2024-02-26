772c3601578a259dd6b0a482aed84058d721a167
Kojanzy <nwmarinoto@gmail.com>
2024-02-26 01:46:53 -0500

    merge `shoalExt` into local repository, likely doesn't work with new 3.8.0 patch

---
+++ patch/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace shoal
+{
+    [BepInPlugin("com.nwmarino.shoal", "shoal", "2.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new InitAllExfiltrationPointsPatch().Enable();
+            new ScavExfiltrationPointPatch().Enable();
+
+            Logger.LogInfo($"Exit patch has run successfully.");
+        }
+    }
+}
